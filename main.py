d1b356dea517ca91de514b4780e0eccf9781a732
Minervus00 <fayouedraogo@protonmail.com>
2024-10-03 17:31:55 +0000

    Rockwell

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ with col1:
              use_column_width="auto")
 
     st.html("""<h1 style='font-weight:bold; text-align:center;
-            font-family:rockwell; margin-left: 2vw'>
+            font-family:Rockwell; margin-left: 2vw'>
             Hi, I'm AiLA !
             </h1>""")
 
